2343685a2da7cdecdac83a19cc1b6f3cfab567bf
mosop <mosop@users.noreply.github.com>
2017-07-23 17:58:32 +0900

    Crystal 0.23.1

--- spec/capture_spec.cr
+++ spec/capture_spec.cr
@@ -9,17 +9,17 @@ module StdioCaptureFeature
         io.in.puts ":P"
         [io.out.gets, io.err.gets, STDIN.gets]
       end
-      out.should eq ":)\n"
-      err.should eq ":(\n"
-      in.should eq ":P\n"
+      out.should eq ":)"
+      err.should eq ":("
+      in.should eq ":P"
     end
 
     it "keeps capturing" do
       Stdio.capture do |io|
         STDOUT.puts ":)"
-        io.out!.gets.should eq ":)\n"
+        io.out!.gets.should eq ":)"
         STDOUT.puts ":X"
-        io.out!.gets.should eq ":X\n"
+        io.out!.gets.should eq ":X"
       end
     end
   end
